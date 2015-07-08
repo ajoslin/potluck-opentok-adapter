7b6f973a16424b6409d96447bc7df7becce92ad2
Andrew Joslin <andrewtjoslin@gmail.com>
2015-07-08 12:23:00 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var test = require('tape')
+// var potluckOpentokAdapter = require('./')
+
+test(function (t) {
+  console.warn('TODO how to test opentok loaded from a cdn, or mock it?')
+  t.end()
+})
